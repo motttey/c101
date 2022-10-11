ad804401630e82506fb6eda492aa20a5991b90f6
motttey <motitago@gmail.com>
2022-10-11 21:26:51 +0900

    install: tailwind.css

--- c101/app.vue
+++ c101/app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <!--
     <NuxtWelcome />
+    -->
+    <p class="text-8xl">TailWind.css</p>
   </div>
 </template>
---
+++ c101/tailwind.config.js
@@ -0,0 +1,5 @@
+{
+    content: [
+        '/*.{html,js, vue}'
+    ] 
+}
