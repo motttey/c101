ebada03a2005dff135da9a1e5c31072a647231d0
motttey <motitago@gmail.com>
2022-10-12 22:40:47 +0900

    fix: show newline

--- c101/app.vue
+++ c101/app.vue
@@ -1,8 +1,9 @@
 <template>
-  <div>
-    <!--
-    <NuxtWelcome />
-    -->
+  <div class="p-6 max-w-sm mx-auto flex items-center space-x-4">
     <p class="text-8xl">TailWind.css</p>
+
+  </div>
+  <div class="p-6 max-w-sm mx-auto flex items-center space-x-4">
+    <p class="text-8xl">AAA</p>
   </div>
 </template>
