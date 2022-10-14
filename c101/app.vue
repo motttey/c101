6471b53c9368fdd5a94bebbd7675c886eb06eb9e
motttey <motitago@gmail.com>
2022-10-14 22:53:30 +0900

    fix: add v-for

--- c101/app.vue
+++ c101/app.vue
@@ -6,4 +6,21 @@
   <div class="p-6 max-w-sm mx-auto flex items-center space-x-4">
     <p class="text-8xl">AAA</p>
   </div>
+
+  <div class="p-6 grid grid-cols-4 gap-4">
+    <div class="space-x-4" v-for="(d, i) in arr" v-bind:key="i"> {{ d }}</div>
+  </div>
 </template>
+
+<script lang="ts">
+export default defineComponent({
+  async setup() {
+    const arr = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9
+    ]
+    return {
+      arr,
+    }
+  },
+})
+</script>
