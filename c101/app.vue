b2ebdaae92fd2f12a59c51e1bb001f95549aeab7
motttey <motitago@gmail.com>
2022-10-16 20:43:02 +0900

    fix: using grid to centering img

--- c101/app.vue
+++ c101/app.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="flex flex-row min-h-screen justify-center items-center">
-    <div class="p-6 max-w-sm mx-auto flex items-center space-x-4">
-      <p class="text-8xl">TailWind.css</p>
-
-    </div>
-    <div class="p-6 max-w-sm mx-auto flex items-center space-x-4">
-      <p class="text-8xl">AAA</p>
+  <div class="flex flex-row min-h-screen justify-center items-center h-screen bg-gradient-to-r from-sky-500 to-indigo-100">
+    <div class="h-screen grid grid-cols-2">
+      <div class="p-6 max-w-sm items-center text-center	space-x-4">
+        <img class="object-none h-screen" src="https://motttey.github.io/perfectblue.jpg" alr="cover">
+      </div>
+      <div class="p-6 items-center align-middle	space-x-4 h-screen">
+        <p class="text-8xl">TailWind.css</p>
+        <p class="text-8xl">AAA</p>
+      </div>
     </div>
   </div>
   <div class="flex flex-row min-h-screen justify-center items-center">
