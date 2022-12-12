11fa401e9bb0f61c334d12a58f94b09fd0768158
motttey <motitago@gmail.com>
2022-12-12 21:12:20 +0900

    fix: baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    app: {
+        baseURL: '/c101-landing'
+    },
     modules: [
         '@nuxtjs/tailwindcss'
     ]
