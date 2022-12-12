8555887c9103cab508abbbbd7a211b1fb3f006a6
motttey <motitago@gmail.com>
2022-12-12 21:49:11 +0900

    fix: nuxt deploy settings in .github/workflows

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     ssr: false,
+    target: "static",
     router: {
         base: '/c101-landing/',
     },
