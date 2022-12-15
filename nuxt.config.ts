7ea8327b30208b9bb6994dd08a3e82bf6a6c2aa2
motttey <motitago@gmail.com>
2022-12-15 09:16:31 +0900

    fix: add title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,8 @@ export default defineNuxtConfig({
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+2:wght@100;200;300;400;500;600;700;800;900&display=swap' }
      
-    ]
+    ],
+    meta: {
+        title: '藤子不二雄オマージュマガジン フジコ・スピリット'
+    }
 })
