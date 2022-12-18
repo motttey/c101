d7c99aa492fa922ae1468ccf067f9bd493c43a7c
motttey <motitago@gmail.com>
2022-12-19 00:25:53 +0900

    fix: html attrs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,31 +6,34 @@ export default defineNuxtConfig({
         '@nuxtjs/tailwindcss'
     ],
     link: [
-        { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+2:wght@100;200;300;400;500;600;700;800;900&display=swap' }
-     
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+2:wght@100;200;300;400;500;600;700;800;900&display=swap' }
     ],
     app:{
-        head: {
-          title: '藤子不二雄オマージュマガジン フジコ・スピリット',
-          meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { name: 'copyright', content: '2020 SAMPLE Co., Ltd.' },
-            { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'robots', name: 'robots', content: 'index, follow'},
-            { hid: 'author', property: 'author', content: '望月田吾作' },
-            { hid: 'keywords', property: 'keywords', content: 'C101,コミックマーケット101,合同誌,ドラえもん,藤子不二雄,コミケ,矢島ともあき,望月田吾作' },
-            { hid: 'description', name: 'description', content: 'コミックマーケット101 サークル「カンヅメカン」によるドラえもんや一次創作合同の紹介ページ' },
-            { hid: 'tw-card', name: 'twitter:card', content: 'summary_large_image'},
-            { hid: 'tw-site', name: 'twitter:site', content: '@mt_tg' },
-            { hid: 'og:url', property: 'og:url', content: 'https://motttey.github.io/c101/' },
-            { hid: 'og:site-name', property: 'og:site_name', content: '藤子不二雄オマージュマガジン フジコ・スピリット' },
-            { hid: 'og:title', property: 'og:title', content: '藤子不二雄オマージュマガジン フジコ・スピリット' },
-            { hid: 'og:type', property: 'og:type', content: 'website' },
-            { hid: 'og:description', property: 'og:description', content: 'コミックマーケット101 サークル「カンヅメカン」によるドラえもんや一次創作合同の紹介ページ' },
-            { hid: 'og:image', property: 'og:image', content: 'https://motttey.github.io/c101/_nuxt/hyoushi.d0bb9595.jpg' }
-          ],
+      head: {
+        htmlAttrs: {
+          lang: 'ja',
+          prefix: 'og: http://ogp.me/ns#'
         },
+        title: '藤子不二雄オマージュマガジン フジコ・スピリット',
+        meta: [
+          { charset: 'utf-8' },
+          { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { name: 'copyright', content: '2020 SAMPLE Co., Ltd.' },
+          { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
+          { hid: 'robots', name: 'robots', content: 'index, follow'},
+          { hid: 'author', property: 'author', content: '望月田吾作' },
+          { hid: 'keywords', property: 'keywords', content: 'C101,コミックマーケット101,合同誌,ドラえもん,藤子不二雄,コミケ,矢島ともあき,望月田吾作' },
+          { hid: 'description', name: 'description', content: 'コミックマーケット101 サークル「カンヅメカン」によるドラえもんや一次創作合同の紹介ページ' },
+          { hid: 'tw-card', name: 'twitter:card', content: 'summary_large_image'},
+          { hid: 'tw-site', name: 'twitter:site', content: '@mt_tg' },
+          { hid: 'og:url', property: 'og:url', content: 'https://motttey.github.io/c101/' },
+          { hid: 'og:site-name', property: 'og:site_name', content: '藤子不二雄オマージュマガジン フジコ・スピリット' },
+          { hid: 'og:title', property: 'og:title', content: '藤子不二雄オマージュマガジン フジコ・スピリット' },
+          { hid: 'og:type', property: 'og:type', content: 'website' },
+          { hid: 'og:description', property: 'og:description', content: 'コミックマーケット101 サークル「カンヅメカン」によるドラえもんや一次創作合同の紹介ページ' },
+          { hid: 'og:image', property: 'og:image', content: 'https://motttey.github.io/c101/_nuxt/hyoushi.d0bb9595.jpg' }
+        ],
+      },
     }
 })
