83f158637c76d9de7669ab1bbbdc678e1d755b17
motttey <motitago@gmail.com>
2022-12-22 21:00:24 +0900

    fix: loading order

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,11 +22,6 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'copyright', content: '2020 SAMPLE Co., Ltd.' },
-        { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'robots', name: 'robots', content: 'index, follow'},
-        { hid: 'author', property: 'author', content: '望月田吾作' },
-        { hid: 'keywords', property: 'keywords', content: 'C101,コミックマーケット101,合同誌,ドラえもん,藤子不二雄,コミケ,矢島ともあき,望月田吾作' },
-        { hid: 'description', name: 'description', content: 'コミックマーケット101 サークル「カンヅメカン」によるドラえもんや一次創作合同の紹介ページ' },
         { hid: 'tw-card', name: 'twitter:card', content: 'summary_large_image'},
         { hid: 'tw-site', name: 'twitter:site', content: '@mt_tg' },
         { hid: 'og:url', property: 'og:url', content: 'https://motttey.github.io/c101/' },
@@ -34,7 +29,12 @@ export default defineNuxtConfig({
         { hid: 'og:title', property: 'og:title', content: '藤子不二雄オマージュマガジン フジコ・スピリット' },
         { hid: 'og:type', property: 'og:type', content: 'article' },
         { hid: 'og:description', property: 'og:description', content: 'コミックマーケット101 サークル「カンヅメカン」によるドラえもんや一次創作合同の紹介ページ' },
-        { hid: 'og:image', property: 'og:image', content: 'https://motttey.github.io/c101/_nuxt/hyoushi.d0bb9595.jpg' }
+        { hid: 'og:image', property: 'og:image', content: 'https://motttey.github.io/c101/_nuxt/hyoushi.d0bb9595.jpg' },
+        { hid: 'viewport', name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'robots', name: 'robots', content: 'index, follow'},
+        { hid: 'author', property: 'author', content: '望月田吾作' },
+        { hid: 'keywords', property: 'keywords', content: 'C101,コミックマーケット101,合同誌,ドラえもん,藤子不二雄,コミケ,矢島ともあき,望月田吾作' },
+        { hid: 'description', name: 'description', content: 'コミックマーケット101 サークル「カンヅメカン」によるドラえもんや一次創作合同の紹介ページ' }
       ],
     },
   }
