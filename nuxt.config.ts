4ec05eaf14cd842dd6bd351d0477f62aad5ef733
motttey <motitago@gmail.com>
2022-12-22 20:59:35 +0900

    fix: nuxt.config.js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false,
-  target: "server",
+  ssr: true,
+  target: "static",
   modules: [
     '@nuxtjs/tailwindcss'
   ],
@@ -9,6 +9,9 @@ export default defineNuxtConfig({
     { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+2:wght@100;200;300;400;500;600;700;800;900&display=swap' }
   ],
+  render: {
+    resourceHints: false
+  },
   app:{
     head: {
       htmlAttrs: {
