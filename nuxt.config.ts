27ebfd7f0aea7c4eba7d6997adc7bfa9e828c599
motttey <motitago@gmail.com>
2025-12-02 23:49:28 +0900

    fix: add nojekyll

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss'
   ],
   app:{
-    baseURL: '/',
+    baseURL: '/c101',
     head: {
       link: [
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
