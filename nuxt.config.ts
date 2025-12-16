559f671a0af6bc37c3106f8602ab9bf00c057c92
motttey <motitago@gmail.com>
2025-12-16 19:35:53 +0900

    Revert "fix: baseURL"
    This reverts commit 7aa0217ff12e79f243a48b717cc55fbf155b4529.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     '@nuxtjs/tailwindcss'
   ],
   app:{
+    baseURL: '/c101',
     head: {
       link: [
         { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
